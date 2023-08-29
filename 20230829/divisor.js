7d7368609c3448edf5d2aa6989f0ea69729d239f
Tultae <toy9987@naver.com>
2023-08-29 21:58:41 +0900

    0829

---
+++ 20230829/divisor.js
@@ -0,0 +1,12 @@
+// array의 각 element 중 divisor로 나누어 떨어지는 값을 오름차순으로 정렬한 배열을 반환하는 함수, solution을 작성해주세요.
+// divisor로 나누어 떨어지는 element가 하나도 없다면 배열에 -1을 담아 반환하세요.
+
+function solution(arr, divisor) {
+  const newArr = arr.filter(number => number % divisor === 0);
+  if (!newArr.length) return [-1];
+  return newArr.sort((a, b) => a - b);
+}
+
+console.log(solution([5, 9, 7, 10], 5)); // [5, 10]
+console.log(solution([2, 36, 1, 3], 1)); // [1, 2, 3, 36]
+console.log(solution([3, 2, 6], 10)); // [-1]
