e94c84cb328edacb1796d54eab222b581450f878
Tultae <toy9987@naver.com>
2023-09-08 21:09:49 +0900

    📕 0908

---
+++ 20230908/correct-().js
@@ -0,0 +1,20 @@
+// 괄호가 바르게 짝지어졌다는 것은 '(' 문자로 열렸으면 반드시 짝지어서 ')' 문자로 닫혀야 한다는 뜻입니다. 예를 들어
+
+// "()()" 또는 "(())()" 는 올바른 괄호입니다.
+// ")()(" 또는 "(()(" 는 올바르지 않은 괄호입니다.
+// '(' 또는 ')' 로만 이루어진 문자열 s가 주어졌을 때, 문자열 s가 올바른 괄호이면 true를 return 하고, 올바르지 않은 괄호이면 false를 return 하는 solution 함수를 완성해 주세요.
+
+function solution(s) {
+  if (s[0] === ')' || s.length % 2 !== 0) return false;
+  return (
+    [...s].reduce((acc, char) => {
+      if (acc < 0) return acc;
+      return char === '(' ? acc + 1 : acc - 1;
+    }, 0) === 0
+  );
+}
+
+console.log(solution('()()')); // t
+console.log(solution('(())()')); // t
+console.log(solution(')()(')); // f
+console.log(solution('(()(')); // f
